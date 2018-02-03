d893b6c750181dd3740ff4857ad72efc55877240
HoboBleach <33799992+HoboBleach@users.noreply.github.com>
2018-02-03 21:55:26 +1100

    Changes in code

--- Usernames.py
+++ Usernames.py
@@ -1,28 +1,33 @@
-import time
 import os
-FileUser = open("Usernames.txt","r")
-#Lines = FileUser.readlines(1)
-print(Lines)
-CUser = str(input("Username: "))
-cuser = str.lower(CUser)
-for x in FileUser:
-    if x == cuser:
-        if cuser == "circlegame" or cuser == "circle_game":
-            print("Nice try")
-            print("""
-               `+mmho/-  
-            `/hNMMMMMMd. 
-        `-+hNMMMMMMMMM-  
-    `:yhdMMMMMMMMMMMMd   
-  -sdMMMMMMMNsydMMMMm.   
- .oohMMMMMMM:  `sMMM-    
-  +mmodMNMMM+`  hMMy     
- smo`oMm--odNdo+MMd.     
- `` /Mm.   `./sdNs`      
-    dd.        ``
-""")
+import sys
+Username = str(input("Enter Username: "))
+LUser = str.lower(Username)
+contents = []
+def RFile():
+    F = open("Username.txt","r")
+    if F.mode == "r":
+        content = F.readlines() """Wont go outside function"""
+        print(content)
+        return contents
+    else:
+        print("Fail")
+        sys.exit
+    F.close
+RFile()
+
+print(contents)
+print(LUser)
+os.system("pause")
+
+
+for x in contents:
+    print(x)
+    os.system("pause")
+    if x == LUser:
+        if LUser == "circlegame" or LUser == "circle_game":
+            print("Nice Try")
         else:
             print("Invalid Username")
     else:
-        print("null")
-time.sleep(100)
+        print("Fail")
+        os.system("pause")
