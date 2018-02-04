5eb595b09d06fc8560d56115ed0b62e08623a869
Winston Tran <winston.tran@thedropbears.org.au>
2018-02-04 12:30:36 +1100

    Functioning Username

--- Usernames.py
+++ Usernames.py
@@ -1,33 +1,64 @@
 import os
 import sys
+import time
+import pybase64
 Username = str(input("Enter Username: "))
 LUser = str.lower(Username)
 contents = []
+Names = []
 def RFile():
-    F = open("Username.txt","r")
+    F = open("Usernames.csv","r")
     if F.mode == "r":
-        content = F.readlines() """Wont go outside function"""
-        print(content)
-        return contents
+        global contents
+        contents = F.readlines()
     else:
         print("Fail")
         sys.exit
     F.close
+def NSpace():
+    for names in contents:
+        Names.append(names.rstrip())
+def Shutdown():
+    print("Shutdown in 10")
+    time.sleep(1)
+    print("9")
+    time.sleep(1)
+    print("8")
+    time.sleep(1)
+    print("7")
+    time.sleep(1)
+    print("6")
+    time.sleep(1)
+    print("5")
+    time.sleep(1)
+    print("4")
+    time.sleep(1)
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
 RFile()
-
-print(contents)
-print(LUser)
-os.system("pause")
-
-
-for x in contents:
-    print(x)
-    os.system("pause")
-    if x == LUser:
+NSpace()
+for x in Names:
+    if LUser == x:
         if LUser == "circlegame" or LUser == "circle_game":
             print("Nice Try")
+            print("""
+              _ _
+           .-/ / )
+           |/ / /
+           /.' /
+          // .---.
+         /   .--._\ 
+        /    `--' /
+       /     .---'
+      /    .'
+          /
+            """)
+            Shutdown()
         else:
             print("Invalid Username")
+            Shutdown()
     else:
-        print("Fail")
-        os.system("pause")
+        None
