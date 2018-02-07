38b0c214379c73407024ddef85498ed04cbd6221
Winston Tran <winston.tran@thedropbears.org.au>
2018-02-07 20:47:57 +1100

    Readme file and module-checker

---
+++ README.txt
@@ -0,0 +1,6 @@
+Requires:
+	1.Scipy
+	2.Numpy
+	3.Matplotlib
+	5.Pandas
+	4.sklearn
---
+++ module-checker.py
@@ -0,0 +1,14 @@
+import sys
+import time
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+print("Python: {}".format(sys.version))
+print("Scipy: {}".format(scipy.__version__))
+print("Numpy: {}".format(numpy.__version__))
+print("Matplotlib: {}".format(matplotlib.__version__))
+print("Pandas: {}".format(pandas.__version__))
+print("Sklearn: {}".format(sklearn.__version__))
+time.sleep(5)
